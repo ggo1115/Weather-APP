4178af0b26d2b3d38eb0f3602442d86892c001ff
ParkJYeon <eternity2808@naver.com>
2018-05-14 00:56:15 +0900

    Permission(ACCESS_FINE_LOCATION) 관련 코드

--- app/src/main/java/com/smu/oss14/bb/weather_app/MainActivity.java
+++ app/src/main/java/com/smu/oss14/bb/weather_app/MainActivity.java
@@ -1,7 +1,13 @@
 package com.smu.oss14.bb.weather_app;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //권한설정 관련 확인 코드
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            //안드로이드 : 마시멜로 이상인 경우
+            boolean ckPermission = checkPermission();
+            if (ckPermission == true){
+                //권한 설정 승인되어 있는 경우
+            }else{
+                //권한 설정 없는 경우
+                String[] pms = {Manifest.permission.ACCESS_FINE_LOCATION};
+                requestPermissions(pms,11);
+            }
+        }else{
+            //안드로이드 : 마시멜로우 하위 버전인 경우
+        }
+
+    }
+
+    //위치권한 허용 여부 확인
+    private boolean checkPermission(){
+        int locationPermission = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION);
+
+        if(locationPermission == PackageManager.PERMISSION_GRANTED){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(requestCode == 11){
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+            }
+        }
     }
 }
