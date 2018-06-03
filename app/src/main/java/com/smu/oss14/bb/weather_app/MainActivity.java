f286dd06184d9de32784d9e33dd1838289167d51
ParkJYeon <eternity2808@naver.com>
2018-06-04 02:44:27 +0900

    날씨아이콘 띄우기 및 날짜정보 받아오기

--- app/src/main/java/com/smu/oss14/bb/weather_app/MainActivity.java
+++ app/src/main/java/com/smu/oss14/bb/weather_app/MainActivity.java
@@ -2,6 +2,8 @@ package com.smu.oss14.bb.weather_app;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -19,7 +21,11 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.io.IOException;
 import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 import okhttp3.Response;
 
@@ -29,15 +35,7 @@ public class MainActivity extends AppCompatActivity{
     Button BtngtSetting;
     TextView TxtLo;
     ImageView weatherImage;
-    TextView TxtTpC;
-    TextView TxtTpR;
-    TextView TxtTMM;
-    TextView TxtTSn;
-    TextView TxtWind;
-    TextView TxtPer;
-    TextView TxtReh;
-    TextView TxtPm10;
-    TextView TxtPm25;
+    TextView TxtTpC, TxtTpR, TxtTMM, TxtTSn, TxtWind, TxtPer, TxtReh, TxtPm10, TxtPm25;
 
     private boolean SetTempScale = true;
     private boolean CurLocationOK = true;
@@ -45,6 +43,11 @@ public class MainActivity extends AppCompatActivity{
     Location_Data LData = new Location_Data();
     Air_Data_PM10 Pm10Data;
     Air_Data_PM25 Pm25Data;
+
+    //long curTime;
+    //Date curDate;
+    //SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
+
 
     ArrayList<Weatherinfo_Data> WDataList = new ArrayList<Weatherinfo_Data>();
 
@@ -65,7 +68,23 @@ public class MainActivity extends AppCompatActivity{
         TxtReh = (TextView) findViewById(R.id.Reh);
         TxtPm10 = (TextView) findViewById(R.id.AirPM10);
         TxtPm25 = (TextView) findViewById(R.id.AirPM25);
+        weatherImage = (ImageView) findViewById(R.id.WeatherImage);
 
+        //curTime = System.currentTimeMillis();
+        //curDate = new Date(curTime);
+        //Log.e("오늘날짜", dateFormat.format(curDate));
+
+        GregorianCalendar today = new GregorianCalendar();
+        GregorianCalendar yesterday = new GregorianCalendar();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
+        yesterday.add(Calendar.DAY_OF_MONTH, -1);
+        String tday = dateFormat.format(today.getTime());
+        String yester = dateFormat.format(yesterday.getTime());
+
+        Log.e("어제/오늘", yester + " / " + tday);
+
+
+        final Handler handler = new Handler();
 
         final GPSActivity gpsActivity = new GPSActivity(MainActivity.this);
         LData = gpsActivity.UseGPS();
@@ -80,14 +99,13 @@ public class MainActivity extends AppCompatActivity{
                     @Override
                     public void onDataChange(DataSnapshot dataSnapshot) {
                         LData.setLCcode(dataSnapshot.getValue(String.class));
+
                         new Thread(){
                             public void run(){
                                 //ReceiverShortWeather를 통한 날씨파싱시도
                                 ReceiveWeather ReceiveWeather = new ReceiveWeather();
                                 ReceiveAirPM10 ReceiveAirPm10 = new ReceiveAirPM10();
                                 ReceiveAirPM25 ReceiveAirPm25 = new ReceiveAirPM25();
-                                //ReceiveAirPm10.execute("");
-                                //ReceiveAirPm25.execute("");
 
                                 //지역코드값 넘겨줘서 실행(추후 DB통해 넣을 예정)
                                 Response response = ReceiveWeather.XMLloading(LData.getLCcode());
@@ -97,25 +115,61 @@ public class MainActivity extends AppCompatActivity{
                                     WDataList = ReceiveWeather.parsing(response.body().string());
                                     Pm10Data = ReceiveAirPm10.parsingPm10(response_10.body().string());
                                     Pm25Data = ReceiveAirPm25.parsingPm25(response_25.body().string());
-                                    Select_Location_Air SLAir = new Select_Location_Air(LData.getAddrValue()[0], Pm10Data, Pm25Data);
-                                    String[] AirState = SLAir.ReturnAir();
-                                    TxtTpC.setText(WDataList.get(0).getTemp_cur()+"º");
-                                    TxtTMM.setText(WDataList.get(3).getTemp_max()+"/"+WDataList.get(3).getTemp_min()+"º");
                                     Double TempRange = Double.parseDouble( WDataList.get(3).getTemp_max())-Double.parseDouble(WDataList.get(3).getTemp_min());
-                                    TxtTpR.setText("일교차\n" + TempRange);
                                     Double TempSens = 13.12 + (0.6215 * Double.parseDouble(WDataList.get(0).getTemp_cur())) - (11.37 * Math.pow(Double.parseDouble(WDataList.get(0).getWs()), 0.16)) + (0.3965 * Math.pow(Double.parseDouble(WDataList.get(0).getWs()), 0.16) * Double.parseDouble(WDataList.get(0).getTemp_cur()));
                                     DecimalFormat form = new DecimalFormat("#.#");
+                                    Select_Location_Air SLAir = new Select_Location_Air(LData.getAddrValue()[0], Pm10Data, Pm25Data);
+                                    String[] AirState = SLAir.ReturnAir();
+
+                                    TxtTpC.setText(WDataList.get(0).getTemp_cur()+"º");
+                                    TxtTMM.setText(WDataList.get(3).getTemp_max()+"/"+WDataList.get(3).getTemp_min()+"º");
+                                    TxtTpR.setText("일교차\n" + TempRange);
                                     TxtTSn.setText("체감\n" + form.format(TempSens)+"º");
-                                    TxtWind.setText("풍속\n" + WDataList.get(0).getWs()+"m/s");
+                                    TxtWind.setText("풍속\n" + WDataList.get(1).getWs()+"m/s");
                                     TxtReh.setText("습도\n" + WDataList.get(0).getReh()+"%");
                                     TxtPer.setText("강수\n" + WDataList.get(0).getPop()+"%");
                                     TxtPm10.setText("(pm10) " + AirState[0]);
                                     TxtPm25.setText("(pm25) " + AirState[1]);
+
+                                    handler.post(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            switch (WDataList.get(0).getWf()){
+                                                case "맑음":
+                                                    weatherImage.setImageResource(R.drawable.sunny);
+                                                    break;
+                                                case "구름 조금":
+                                                    weatherImage.setImageResource(R.drawable.little_cloud);
+                                                    break;
+                                                case "구름 많음":
+                                                    weatherImage.setImageResource(R.drawable.many_cloud);
+                                                    break;
+                                                case "흐림":
+                                                    weatherImage.setImageResource(R.drawable.cloudy);
+                                                    break;
+                                                case "비":
+                                                    weatherImage.setImageResource(R.drawable.rainy);
+                                                    break;
+                                                case "눈/비":
+                                                    weatherImage.setImageResource(R.drawable.snowrain);
+                                                    break;
+                                                case "눈":
+                                                    weatherImage.setImageResource(R.drawable.snowing);
+                                                    break;
+                                                default:
+                                                    weatherImage.setImageResource(R.drawable.sunny);
+                                            }
+                                        }
+                                    });
+
+
+
                                 } catch (IOException e) {
                                     e.printStackTrace();
                                 }
                             }
                         }.start();
+
                     }
                     @Override
                     public void onCancelled(DatabaseError databaseError) {
@@ -151,6 +205,29 @@ public class MainActivity extends AppCompatActivity{
                 Log.e("현재위치사용?", "선택결과 - " + CurLocationOK);
             }
         }
+    }
+
+    public int SetImage(String weatherState){
+
+        int Imageid = 0;
+
+        if(weatherState == "맑음"){
+            Imageid = R.drawable.sunny;
+        }else if(weatherState == "구름 조금"){
+            Imageid = R.drawable.little_cloud;
+        }else if(weatherState == "구름 많음"){
+            Imageid = R.drawable.many_cloud;
+        }else if(weatherState == "흐림"){
+            Imageid = R.drawable.cloudy;
+        }else if(weatherState == "비"){
+            Imageid = R.drawable.rainy;
+        }else if(weatherState == "눈/비"){
+            Imageid = R.drawable.snowing;
+        }else if(weatherState == "눈"){
+            Imageid = R.drawable.snowing;
+        }
+
+        return Imageid;
     }
 
     public void onClick1(View view){
